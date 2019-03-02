a9f2a728d154b6dce7b1da969ff8c601b63c1a95
clarajmunro@gmail.com <clarajmunro@gmail.com>
2019-03-02 11:20:32 -0800

    3 tests passing for Client Models

--- HairSalon.Tests/ModelTests/ClientTests.cs
+++ HairSalon.Tests/ModelTests/ClientTests.cs
@@ -10,9 +10,41 @@ namespace HairSalon.Tests
    [TestMethod]
     public void ClientConstructor_CreatesInstanceOfClient_Client()
     {
-      Client newClient = new Client();
+      Client newClient = new Client("test");
       Assert.AreEqual(typeof(Client), newClient.GetType());
     }
 
-  }
+   [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Client newClient = new Client(description);
+
+      //Act
+      string result = newClient.GetDescription();
+
+      //Assert
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Client newClient = new Client(description);
+
+      //Act
+      string updatedDescription = "Do the dishes";
+      newClient.SetDescription(updatedDescription);
+      string result = newClient.GetDescription();
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+
+    }
+
 }
