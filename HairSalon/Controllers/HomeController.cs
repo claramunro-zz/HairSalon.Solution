05d22e80cb8bfdad51a73ce292e04cebc6ee116c
clarajmunro@gmail.com <clarajmunro@gmail.com>
2019-03-02 13:11:11 -0800

    addedClients controller tests, home controller tests

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace HairSalon.Controllers
       [HttpGet("/")]
       public ActionResult Index()
       {
-        return View();
+        return View(0);
       }
 
     }
--- HairSalon/Startup.cs
+++ HairSalon/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ToDoList
+namespace HairSalon
 {
   public class Startup
   {
@@ -25,6 +25,7 @@ namespace ToDoList
 
     public void Configure(IApplicationBuilder app)
     {
+      app.UseDeveloperExceptionPage();
       app.UseMvc(routes =>
       {
         routes.MapRoute(
