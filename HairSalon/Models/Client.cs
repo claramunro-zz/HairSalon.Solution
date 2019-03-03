ad23023b4d27d5a0864f09e5c1e5b3b097d65581
clarajmunro@gmail.com <clarajmunro@gmail.com>
2019-03-02 18:18:51 -0800

    refactoring for database

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -5,15 +5,14 @@ namespace HairSalon.Models
   public class Client
   {
    private string _description;
-   private int _id;
-   private static List<Client> _instances = new List<Client> {};
+   // private int _id;
 
     public Client (string description)
       {
         _description = description;
-        _id = _instances.Count;
-        _instances.Add(this);
+        // _id = _instances.Count;
       }
+
 
     public string GetDescription()
       {
@@ -25,25 +24,44 @@ namespace HairSalon.Models
         _description = newDescription;
       }
 
-    public int GetId()
-      {
-        return _id;
-      }
+    // public int GetId()
+    //   {
+    //     return _id;
+    //   }
 
     public static List<Client> GetAll()
-    {
-        return _instances;
-    }
+      {
+      List<Client> allClients = new List<Client> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int itemId = rdr.GetInt32(0);
+        string clientDescription = rdr.GetString(1);
+        Client newClient = new Client(clientDescription, clientId);
+        allClients.Add(newClient);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      
+      return allClients;
+      }
 
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
+    // public static void ClearAll()
+    // {
+    //   _instances.Clear();
+    // }
 
-     public static Client Find(int searchId)
-    {
-      return _instances[searchId-1];
-    }
+    //  public static Client Find(int searchId)
+    // {
+    //   return _instances[searchId-1];
+    // }
 
 
   }
